7392dfa00bdc83d4cf651705113a6089e1845802
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-01-21 12:07:27 +0500

    GrowHub.co

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,19 @@ import ReactDOM from "react-dom/client"
 import { ApolloProvider } from "@apollo/client"
 import { BrowserRouter as Router } from "react-router-dom"
 import { ThemeProvider } from "./contexts/ThemeContext"
-import { Toaster } from "react-hot-toast"
 import App from "./App.jsx"
 import "./index.css"
 import client from "./apollo-client"
-import ParticleBackground from "./components/ParticleBackground"  // Import the ParticleBackground component
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <ThemeProvider>
         <Router>
-          {/* Particle Background added here */}
-          <div className="relative">
-            <ParticleBackground />  {/* Add the background particles */}
-            <App />
-          </div>
+          <App />
         </Router>
       </ThemeProvider>
     </ApolloProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
+
