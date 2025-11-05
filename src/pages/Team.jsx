06cb7b66033590dc1082e86d854e5a9ac370af58
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-11-05 11:22:50 +0500

    Nexovate Digital

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -25,7 +25,7 @@ const teamMembers = [
     name: "Himesh Dua",
     role: "Backend Developer | Database & API Engineer",
     image:
-      "src/assets/Team-Member-images/1760013021624.jpg",
+      "assets/Team-Member-images/1760013021624.jpg",
     description:
       "Himesh builds reliable, secure, and scalable backend systems using Node.js, Laravel, and .NET Core. He manages APIs, databases, and integrations that power every digital solution.",
     portfolio: "https://himeshdua.vercel.app/",
